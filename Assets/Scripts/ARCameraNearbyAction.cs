d5e45e6f0b1e3e8104fb4aae8b06dfc0e5391bef
Eleven Liu <jiejyunliu@gmail.com>
2023-12-25 20:27:45 +0800

    Add ARCameraNearbyAction

---
+++ Assets/Scripts/ARCameraNearbyAction.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCameraNearbyAction : MonoBehaviour {
+    private ARCameraManager arCameraManager;
+    public Transform arCameraTransform; // Reference to the AR camera's transform
+    public float hideDistance = 0.1f;    // The distance at which the GameObject should be hidden
+
+
+    void OnEnable(){
+        arCameraManager = FindObjectOfType<ARCameraManager>();
+        if (arCameraManager != null){
+            arCameraTransform = arCameraManager.transform;
+            Debug.Log("Get AR Camera.");
+        } else {
+            Debug.LogError("AR Camera transform not assigned and AR Camera Manager not found.");
+        }
+    }
+
+    private void Update() {
+        if (arCameraManager != null) {
+            // Log the camera position
+            // Vector3 cameraPosition = arCameraManager.transform.position;
+            Debug.Log("AR Camera Position: " + arCameraTransform.position);
+
+            // Calculate the distance between the AR camera and this GameObject
+            float distanceToCamera = Vector3.Distance(arCameraTransform.position, transform.position);
+            Debug.Log("Distance to camera " + distanceToCamera);
+
+            // Check if the distance is less than the hideDistance
+            if (distanceToCamera < hideDistance) {
+                Debug.LogError("Hit!");
+                SetObjectVisibility(false);
+            } else {
+                SetObjectVisibility(true);
+            }
+        }
+    }
+
+    private void SetObjectVisibility(bool isVisible) {
+        // You can use Renderer component to set visibility. If the object has a Renderer component:
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null) {
+            renderer.enabled = isVisible;
+        }
+
+        // Alternatively, you can use GameObject.SetActive to enable or disable the entire GameObject:
+        // gameObject.SetActive(isVisible);
+    }
+}
